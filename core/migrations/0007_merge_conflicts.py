19f393ad1e068c0b57a36e0aeaaa845d04a1fa8b
amalinahashim <nowtheworldisflat@hotmail.com>
2024-06-13 03:35:28 +0800

    added dockerfile

---
+++ core/migrations/0007_merge_conflicts.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+        ('core', '0006_add_missing_fields'),
+    ]
+
+    operations = [
+    ]
