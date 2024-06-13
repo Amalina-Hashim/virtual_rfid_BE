b0294b7bf2c25bf4a06850705affd916b6a3a997
amalinahashim <nowtheworldisflat@hotmail.com>
2024-06-13 09:00:19 +0800

    updated host in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'testserver',  
+    'testserver',
+    'django-app-kkoytosj3a-as.a.run.app',   
 ]
 
 # Application definition
